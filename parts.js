3832e0769c19b22069d4702eb942802c61a6d4eb
yawatasirazunomori <uruunisii@gmail.com>
2026-06-01 22:25:18 +0900

    ページをアップロード
    黄泉市のページをアップロード

---
+++ parts.js
@@ -0,0 +1,31 @@
+$(function () {
+    $(".common_header").load("/yomi-city/parts/header.html");
+});
+
+$(function () {
+    $(".common_hero_header").load("/yomi-city/parts/hero-header2.html");
+});
+
+$(function () {
+    $(".common_information").load("/yomi-city/parts/information.html");
+});
+
+$(function () {
+    $(".common_menu2").load("/yomi-city/parts/menu2.html");
+});
+
+$(function () {
+    $(".common_side").load("/yomi-city/parts/side.html");
+});
+
+$(function () {
+    $(".common_related_sites").load("/yomi-city/parts/related-sites.html");
+});
+
+$(function () {
+    $(".common_AD").load("/yomi-city/parts/AD.html");
+});
+
+$(function () {
+    $(".common_footer").load("/yomi-city/parts/footer.html");
+});
---
+++ yomi.js
@@ -0,0 +1,23 @@
+/*
+document.querySelector('.accordion-head').addEventListener('click', function() {
+  const body = this.nextElementSibling;
+  if (body.style.display === 'none') {
+    body.style.display = 'flex';
+  } else {
+    body.style.display = 'none';
+  }
+});
+
+document.onclick=function() {
+  var obj = document.getElementById('open').style;
+  obj.display = (obj.display == 'none') ? 'flex' : 'none';
+};
+
+
+document.querySelector('.h-menu').addEventListener('click',function() {
+  var openMenu = document.getElementByClassName('h-menu');
+  alert("クリックされた")
+  openMenu.style.display = (openMenu.style.display == 'none') ? 'flex' : 'none';
+  
+});
+*/
